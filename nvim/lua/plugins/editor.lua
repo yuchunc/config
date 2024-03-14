ad22ce9a3977b5cfdab7cb1981f11456400c8dd8
Mickey Chen <poverwhelming@gmail.com>
2024-03-14 14:16:47 +0800

    added plugin editor.lua & disabled formating for some exts

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    enabled = false,
+    "folke/flash.nvim",
+    ---@type Flash.Config
+    opts = {
+      search = {
+        forward = true,
+        multi_window = false,
+        wrap = false,
+        incremental = true,
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            -- disable fuzzy finder
+            ["/"] = "noop",
+          },
+        },
+      },
+    },
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = {
+      highlighters = {
+        -- This highlights hsl.
+        hsl_color = {
+          pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
+          group = function(_, match)
+            local utils = require("solarized-osaka.hsl")
+
+            --- @type string, string, string
+            local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
+
+            --- @type number?, number?, number?
+            local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+
+            --- @type string
+            local hex_color = utils.hslToHex(h, s, l)
+
+            return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+          end,
+        },
+      },
+    },
+  },
+}
