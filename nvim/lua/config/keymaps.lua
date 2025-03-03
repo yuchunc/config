5de6724eb33255da2166575c5f9785141415f60a
Mickey Chen <hey.mickabo@gmail.com>
2025-03-03 12:15:00 +0800

    updated lazyvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,22 +1,43 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set("n", "+", "<C-a>")
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>", opts)
+keymap.set("n", "-", "<C-x>", opts)
 
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Move lines up and down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
-keymap.set("n", "<C-a>", "gg<C-i>", opts)
+-- Jump to start
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 
-keymap.set("n", "te", "tabeidt", opts)
+-- Window navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
 
--- Split window
-keymap.set("n", "sh", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Window management
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
+keymap.set("n", "<leader>sh", ":split<CR>", opts)
 
--- New tab
-keymap.set("n", "te", ":tabedit")
+-- Tabs
+keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>", opts) -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>", opts) -- go to previous tab
+
+-- Buffer navigation
+keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Stay in indent mode
+keymap.set("v", "<", "<gv", opts)
+keymap.set("v", ">", ">gv", opts)
+
+-- Clear highlights
+keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
